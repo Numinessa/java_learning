a74d950113ba095ef355e2c2b6dd7aca684d359c
Agnieszka <budzynska.agnieszka1988@gmail.com>
2017-07-30 12:50:35 +0200

    Dodano kilka funkcji

--- Java/Dev/sandbox/src/main/java/ru/stqa/pft/sandbox/Hello.java
+++ Java/Dev/sandbox/src/main/java/ru/stqa/pft/sandbox/Hello.java
@@ -3,7 +3,25 @@ package ru.stqa.pft.sandbox;
 public class Hello {
 	public static void main(String args[])
 	{
-    	System.out.println("ru.stqa.pft.sandbox.Hello, world!");
+    	hello("world");
+    	hello("user");
+    	hello("Aga");
+    	double l = 5;
+		System.out.println("Powierzchnia kwadratu o boku "+ l +" = "+ area(l));
+
+		double a = 4;
+		double b = 6;
+		System.out.println("Powierzchnia prostokonta o boku "+ a +" i "+ b +" = "+ area(a, b));
+	}
+	public static void hello(String somebody){
+		System.out.println("Hello," + somebody +"!");
+	}
+	public static double area (double len){
+		return len*len;
+	}
+	public static double area (double a, double b){
+		return a * b;
+
 	}
 }
 
