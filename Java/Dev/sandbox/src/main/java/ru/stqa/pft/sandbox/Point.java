a3977b04c448cba4851166666a1c98d609fa016a
Agnieszka <budzynska.agnieszka1988@gmail.com>
2017-07-31 18:20:58 +0200

    HomeWork2

--- Java/Dev/sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ Java/Dev/sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -1,18 +1,20 @@
 package ru.stqa.pft.sandbox;
 
 public class Point {
-  public double p1 = 5;
-  public double  p2 = 10;
+  public double p1;
+  public double p2;
 
- // public static double distance(Point p1, Point p2) {
-  //  this.p1 = p1;
-  //  this p2 = p2;
-  //  return this.p2 * this p1;
+  public Point(double p1, double p2) {
+   this.p1 = p1;
+   this.p2 = p2;
+  }
 
- // }
+ // public static double distance(Point p1, Point p2){
+ //   return Math.sqrt();
+//  }
+
   public static void main(String[] args){
-
-    Point p = new Point();
-    System.out.println("cos " + p.p1 +"i "+ p.p2 + " = " + Math.sqrt(p.p1 * p.p2));
+    Point p = new Point(10, 5);
+    System.out.println("Wyliczenie odleglosci miedzy dwoma punktami: " + p.p1 +" i "+ p.p2 + " = " + Math.sqrt(p.p1 * p.p2));
   }
 }
