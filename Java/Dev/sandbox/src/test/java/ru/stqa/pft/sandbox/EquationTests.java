ef26aefd3481d92c114d865f16006abb9f079831
Agnieszka <budzynska.agnieszka1988@gmail.com>
2017-08-13 23:38:22 +0200

    Testy z If else

--- Java/Dev/sandbox/src/test/java/ru/stqa/pft/sandbox/EquationTests.java
+++ Java/Dev/sandbox/src/test/java/ru/stqa/pft/sandbox/EquationTests.java
@@ -6,20 +6,35 @@ import org.testng.annotations.Test;
 public class EquationTests {
 
   @Test
-  public void Test0() {
+  public void test0() {
     Equation e = new Equation(1,1,1);
     Assert.assertEquals(e.rootNumber(), 0);
   }
 
   @Test
-  public void Test1() {
+  public void test1() {
     Equation e = new Equation(1,2,1);
     Assert.assertEquals(e.rootNumber(), 1);
   }
 
   @Test
-  public void Test3() {
+  public void test3() {
     Equation e = new Equation(1,5,6);
     Assert.assertEquals(e.rootNumber(), 2);
+  }
+  @Test
+  public void testLinear() {
+    Equation e = new Equation(0,1,1);
+    Assert.assertEquals(e.rootNumber(), 1);
+  }
+  @Test
+  public void testConstant() {
+    Equation e = new Equation(0,0,1);
+    Assert.assertEquals(e.rootNumber(), 0);
+  }
+  @Test
+  public void testZero() {
+    Equation e = new Equation(0,0,0);
+    Assert.assertEquals(e.rootNumber(), -1);
   }
 }
