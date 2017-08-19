7ef2858e32f85cd1e31fe1f6957cfcfbeccddbcf
Agnieszka <budzynska.agnieszka1988@gmail.com>
2017-08-19 13:53:54 +0200

    4.5. Kształtowanie kolekcji

--- Java/Dev/addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
+++ Java/Dev/addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
@@ -2,8 +2,13 @@ package ru.stqa.pft.addressbook.appmanager;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import ru.stqa.pft.addressbook.model.GroupData;
+
+import javax.xml.ws.WebEndpoint;
+import java.util.ArrayList;
+import java.util.List;
 
 public class GroupHelper extends HelperBase {
 
@@ -35,9 +40,9 @@ public class GroupHelper extends HelperBase {
   }
 
   public void selectGroup(int index) {
-  //  if (!wd.findElement(By.name("selected[]")).isSelected()) {
+    //  if (!wd.findElement(By.name("selected[]")).isSelected()) {
     //  click(By.name("selected[]"));
-   // }
+    // }
     wd.findElements(By.name("selected[]")).get(index).click();
   }
 
@@ -63,5 +68,16 @@ public class GroupHelper extends HelperBase {
 
   public int getGroupCount() {
     return wd.findElements(By.name("selected[]")).size();
+  }
+
+  public List<GroupData> getGroupList() {
+    List<GroupData> groups = new ArrayList<GroupData>();
+    List<WebElement> elements = wd.findElements(By.cssSelector("span.group"));
+    for (WebElement element : elements) {
+      String name = element.getText();
+      GroupData group = new GroupData(name, null, null);
+      groups.add(group);
+    }
+    return groups;
   }
 }
