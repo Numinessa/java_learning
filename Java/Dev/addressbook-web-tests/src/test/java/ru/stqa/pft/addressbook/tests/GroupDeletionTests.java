5b23e4c728354b8855b92f92b9b5141a0cbae57d
Agnieszka <budzynska.agnieszka1988@gmail.com>
2017-08-25 20:04:55 +0200

    6. Porównanie kolekcji

--- Java/Dev/addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletionTests.java
+++ Java/Dev/addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletionTests.java
@@ -22,6 +22,11 @@ public class GroupDeletionTests extends TestBase {
     app.getGroupHelper().retuenToGroupPage();
     List<GroupData> after = app.getGroupHelper().getGroupList();
     Assert.assertEquals(after.size(), before.size() -1);
+
+
+    before.remove(before.size() -1);
+      Assert.assertEquals(before, after);
+
   }
 
 }
