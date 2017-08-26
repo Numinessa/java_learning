2da38b0ca253b3afed3509b58bbb3200702a768d
Agnieszka <budzynska.agnieszka1988@gmail.com>
2017-08-26 17:09:07 +0200

    10. Sortowanie list

--- Java/Dev/addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
+++ Java/Dev/addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
@@ -6,8 +6,10 @@ public class GroupData {
   private final String header;
   private final String footer;
 
-  public GroupData( String name, String header, String footer) {
-    this.id = 0;
+
+  public GroupData(String name, String header, String footer) {
+    this.id = Integer.MAX_VALUE;
+
     this.name = name;
     this.header = header;
     this.footer = footer;
@@ -33,26 +35,6 @@ public class GroupData {
   }
 
 
-
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) return true;
-    if (o == null || getClass() != o.getClass()) return false;
-
-    GroupData groupData = (GroupData) o;
-
-    if (id != groupData.id) return false;
-    return name != null ? name.equals(groupData.name) : groupData.name == null;
-  }
-
-
-  @Override
-  public int hashCode() {
-    int result = id;
-    result = 31 * result + (name != null ? name.hashCode() : 0);
-    return result;
-  }
-
   @Override
   public String toString() {
     return "GroupData{" +
@@ -69,4 +51,19 @@ public class GroupData {
 
     return id;
   }
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+
+    GroupData groupData = (GroupData) o;
+
+    return name != null ? name.equals(groupData.name) : groupData.name == null;
+  }
+
+  @Override
+  public int hashCode() {
+    return name != null ? name.hashCode() : 0;
+  }
+
 }
