f1d7e934468651da2c8d4d9a20fdad4575d78a75
Agnieszka <budzynska.agnieszka1988@gmail.com>
2017-08-28 22:53:11 +0200

    Home Work 10 part 2

--- Java/Dev/addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/AddressModificationTests.java
+++ Java/Dev/addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/AddressModificationTests.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.AddressData;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class AddressModificationTests extends TestBase {
@@ -14,18 +15,26 @@ public class AddressModificationTests extends TestBase {
 
 
     if (! app.getContactHelper().isThereAnyAddress()){
-      app.getContactHelper().createAAddress(new AddressData("Agnieszka", "Sara","Budzyńska","test2", "Ładna 10/15", "555-555-555"));
+      app.getContactHelper().createAAddress(new AddressData("Agnieszka", "Sara","Budzyńska",null, null, null));
     }
 
     List<AddressData> before = app.getContactHelper().getContactList();
 
     app.getContactHelper().selectFirstAddress(before.size() -1);
     app.getContactHelper().clickEditAddress();
-    app.getContactHelper().fillAddressForm(new AddressData("Monika", "Sara","Budzyńska", "test2", "Ładna 10/15", "555-555-555"), false);
+
+    AddressData group = new AddressData("Agnieszka", "Sara","Budzyńska", null, null, null);
+   // app.getContactHelper().fillAddressForm(new AddressData("Monika", "Sara","Budzyńska", "test2", "Ładna 10/15", "555-555-555"), false);
+    app.getContactHelper().fillAddressForm((group), false);
+
     app.getContactHelper().updateButton();
     app.getNavigationHelper().goToHomePage();
 
     List<AddressData> after = app.getContactHelper().getContactList();
     Assert.assertEquals(after.size(), before.size() );
+
+    before.remove(before.size() -1);
+    before.add(group);
+    Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
   }
 }
