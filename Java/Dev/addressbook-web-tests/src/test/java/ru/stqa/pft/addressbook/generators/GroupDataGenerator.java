4e56941afc50f653aa6dcee4f76ad64b818e6429
Agnieszka <budzynska.agnieszka1988@gmail.com>
2017-09-18 19:07:46 +0200

    6. Format XML

--- Java/Dev/addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/GroupDataGenerator.java
+++ Java/Dev/addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/GroupDataGenerator.java
@@ -3,7 +3,7 @@ package ru.stqa.pft.addressbook.generators;
 import com.beust.jcommander.JCommander;
 import com.beust.jcommander.Parameter;
 import com.beust.jcommander.ParameterException;
-import org.junit.runners.Parameterized;
+import com.thoughtworks.xstream.XStream;
 import ru.stqa.pft.addressbook.model.GroupData;
 
 import java.io.File;
@@ -21,6 +21,9 @@ public class GroupDataGenerator {
 
   @Parameter(names = "-f", description = "Target file")
   public String file;
+
+  @Parameter(names = "-d", description = "Target file")
+  public String format;
 
 public static void main(String[] args) throws IOException {
 
@@ -42,10 +45,25 @@ public static void main(String[] args) throws IOException {
   private void run() throws IOException {
 
     List<GroupData> groups = generateGroups(count);
-    save(groups, new File(file));
+    if (format.equals("csv")){
+      saveAsCsv(groups, new File(file));
+    }else if (format.equals("xml")) {
+      saveAsXml(groups, new File(file));
+    }else{
+      System.out.println("Unregnized format" + format);
+    }
   }
 
-  private  void save(List<GroupData> groups, File file) throws IOException {
+  private void saveAsXml(List<GroupData> groups, File file) throws IOException {
+    XStream xstream = new XStream();
+    xstream.processAnnotations(GroupData.class);
+    String xml = xstream.toXML(groups);
+    Writer writer = new FileWriter(file);
+    writer.write(xml);
+    writer.close();
+  }
+
+  private  void saveAsCsv(List<GroupData> groups, File file) throws IOException {
     System.out.println(new File(".").getAbsolutePath());
     Writer writer = new FileWriter(file);
     for (GroupData group : groups){
@@ -60,6 +78,6 @@ public static void main(String[] args) throws IOException {
     groups.add(new GroupData().withName(String.format("test %s", i))
             .withHeader(String.format("header %s", i)).withFooter(String.format("footer %s", i)));
   }
-  return null;
+  return groups;
   }
 }
